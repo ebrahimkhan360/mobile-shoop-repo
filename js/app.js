d9a2fa9e95d351a4cbabe3197f1748e0d5d6fe7f
ebrahimkhan360 <md.ebrahim.khan.w@gmail.com>
2022-03-01 14:24:10 +0600

    titel style and js search menu

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 const searchPhone = () => {
-    const searchField = document.getElementById("search-field");
+    const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    searchField.value = "";
+    searchField.value = '';
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    console.log(url)
     fetch(url)
-        .then(res => res.joson())
+        .then(res => res.json())
         .then(data => displayPhone(data.data))
 }
-const displayPhone = phones => {
-    console.log(phones)
+// search result
+const displayPhone = data => {
+    const searchResult = document.getElementById('search-result')
+    searchResult.textContent = '';
+
+    data.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div onclick="loadPhoneDetail(${data.id})" class="card h-100">
+                    <img src="${data.image}" class="card-img-top" alt="...">
+
+                <div class="card-body">
+                     <h5 class="card-title">${data.brand}</h5>
+                    <p>${data.phone_name}</p>
+                    <p>${data.slug}</p>
+
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
 }
